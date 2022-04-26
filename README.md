db938d46ce37191b9621aff36620c8a7b39f5f74
lorenypsum <loren.ypsum@gmail.com>
2022-04-26 12:56:57 -0300

    Improve documentation

--- README.md
+++ README.md
@@ -18,7 +18,13 @@ For that purpose, we create a `.env` file at the project folder with the followi
 ```env
 DATABASE_URL=database_url_goes_here
 ```
-The database URL is secret and thus is not provided in this README.
+Instead of `database_url_goes_here`,
+insert the URL of the MongoDB database that you wish to use for development.
+If you install MongoDB locally,
+it should probably be something like `mongodb://localhost/...`.
+Alternatively,
+you can use the URL of the remote production database.
+That database URL is secret and thus is not provided in this README.
 If you haven't received it yet, please ask for it.
 
 Now, we can use `npm` (a program that comes bundled with Node.js) to install the project's dependencies.
@@ -35,3 +41,8 @@ npm run dev
 While running in development mode,
 whenever we change a file locally,
 Remix will reload that file and propagate the changes to the browser automatically.
+
+If you use a local MongoDB instance,
+then you will want to create tables and seed the database with the initial data.
+The command `npx prisma db push` creates the tables and indexes,
+and the command `npx prisma db seed` populates it by running the script at `prisma/seed.ts`.
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,41 +1,44 @@
 import { PrismaClient } from "@prisma/client";
 import fs from "fs/promises";
 
-const prisma = new PrismaClient();
-
 async function main() {
-  const file = await fs.readFile("prisma/Kanji.csv", {
-    encoding: "utf-8",
-  });
-  const data = file.split("\n").map((line) => {
-    const [
-      kanji,
-      kunyomi,
-      onyomi,
-      meaning,
-      story1,
-      story2,
-      story3,
-      vocabulary,
-    ] = line
-      .split("\t")
-      .map((str) => str.replaceAll('""', '"'))
-      .map((str) => (str.startsWith('"') ? str.slice(1, str.length - 1) : str));
-    return {
-      kanji,
-      kunyomi,
-      onyomi,
-      meaning,
-      stories: [story1, story2, story3],
-      vocabulary,
-    };
-  });
-  await prisma.kanji.createMany({ data });
-}
-
-main()
-  .catch((error) => {
+  const prisma = new PrismaClient();
+  try {
+    const file = await fs.readFile("prisma/Kanji.csv", {
+      encoding: "utf-8",
+    });
+    const data = file.split("\n").map((line) => {
+      const [
+        kanji,
+        kunyomi,
+        onyomi,
+        meaning,
+        story1,
+        story2,
+        story3,
+        vocabulary,
+      ] = line
+        .split("\t")
+        .map((str) => str.replaceAll('""', '"'))
+        .map((str) =>
+          str.startsWith('"') ? str.slice(1, str.length - 1) : str
+        );
+      return {
+        kanji,
+        kunyomi,
+        onyomi,
+        meaning,
+        stories: [story1, story2, story3],
+        vocabulary,
+      };
+    });
+    await prisma.kanji.createMany({ data });
+  } catch (error) {
     console.error(error);
     process.exit(1);
-  })
-  .finally(() => prisma.$disconnect());
+  } finally {
+    prisma.$disconnect();
+  }
+}
+
+main();
